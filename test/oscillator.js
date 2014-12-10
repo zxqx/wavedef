5606fc1029ddbdb6308f460826706724201f0563
Zak Angelle <zangelle@w2odigital.com>
2014-12-10 14:25:37 -0600

    Favor setting oscillator params via set methods

--- test/oscillator.js
+++ test/oscillator.js
@@ -5,30 +5,40 @@ import Oscillator from '../lib/Oscillator.js';
 test('Set frequency', (t) => {
   t.plan(1);
 
-  var ctx = nm.mock('createOscillator').takes().returns({});
-  var osc = new Oscillator(ctx, 3000, 4, 'square'); 
-  
+  var ctx = nm.mock('createOscillator').takes().returns({
+   frequency: { value: 0 }
+  });
+
+  var osc = new Oscillator(ctx);
+
   osc.setFrequency(500);
 
-  t.strictEquals(500, osc.node.frequency);
+  t.strictEquals(500, osc.node.frequency.value);
 });
 
 test('Set detune', (t) => {
   t.plan(1);
 
-  var ctx = nm.mock('createOscillator').takes().returns({});
-  var osc = new Oscillator(ctx, 3000, 4, 'square'); 
+  var ctx = nm.mock('createOscillator').takes().returns({
+   detune: { value: 0 }
+  });
+
+  var osc = new Oscillator(ctx);
   
+  osc.node.detune.value = 0;
   osc.setDetune(72);
 
-  t.strictEquals(72, osc.node.detune);
+  t.strictEquals(72, osc.node.detune.value);
 });
 
 test('Set waveform type', (t) => {
   t.plan(1);
 
-  var ctx = nm.mock('createOscillator').takes().returns({});
-  var osc = new Oscillator(ctx, 3000, 4, 'square'); 
+  var ctx = nm.mock('createOscillator').takes().returns({
+   type: ''
+  });
+
+  var osc = new Oscillator(ctx);
   
   osc.setWaveformType('sawtooth');
 
@@ -38,19 +48,25 @@ test('Set waveform type', (t) => {
 test('Transition frequency', (t) => {
   t.plan(1);
 
-  var ctx = nm.mock('createOscillator').takes().returns({});
-  var osc = new Oscillator(ctx, 3000, 4, 'square'); 
+  var ctx = nm.mock('createOscillator').takes().returns({
+   frequency: { value: 3000 }
+  });
+
+  var osc = new Oscillator(ctx);
 
   osc.transitionFrequency(4000, 1000)
-    .then(() => t.strictEquals(4000, osc.node.frequency));
+    .then(() => t.strictEquals(4000, osc.node.frequency.value));
 });
 
 test('Transition detune', (t) => {
   t.plan(1);
 
-  var ctx = nm.mock('createOscillator').takes().returns({});
-  var osc = new Oscillator(ctx, 3000, 4, 'square'); 
+  var ctx = nm.mock('createOscillator').takes().returns({
+   detune: { value: 4 }
+  });
+
+  var osc = new Oscillator(ctx);
 
   osc.transitionDetune(26, 2000)
-    .then(() => t.strictEquals(26, osc.node.detune));
+    .then(() => t.strictEquals(26, osc.node.detune.value));
 });
