3b2e2daa1326633c252c940f3a1f36a0a58ebb90
Zak Angelle <zangelle@w2odigital.com>
2014-12-10 16:06:17 -0600

    No early bounce on identical transition values, with tesses

--- test/knob.js
+++ test/knob.js
@@ -1,7 +1,7 @@
 import test from 'tape';
 import Knob from '../lib/Knob.js';
 
-test('Transition value', (t) => {
+test('Transition value up', (t) => {
   t.plan(1);
 
   var knob = new Knob();
@@ -9,4 +9,24 @@ test('Transition value', (t) => {
 
   knob.transitionValue(node, 'value', 100, 1000)
     .then(() => t.strictEquals(node.value, 100));
+});
+
+test('Transition value down', (t) => {
+  t.plan(1);
+
+  var knob = new Knob();
+  var node = { value: 500 };
+
+  knob.transitionValue(node, 'value', 20, 250)
+    .then(() => t.strictEquals(node.value, 20));
+});
+
+test('Transition to identical value', (t) => {
+  t.plan(1);
+
+  var knob = new Knob();
+  var node = { value: 200 };
+
+  knob.transitionValue(node, 'value', 200, 500)
+    .then(() => t.strictEquals(node.value, 200));
 });
