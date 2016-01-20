c79b75860c06b0a6472989868cf07928d946b050
Drake Champagne <trivorak@gmail.com>
2016-01-19 22:42:23 -0600

    Filter added but needs to be fixed

--- examples/main.js
+++ examples/main.js
@@ -1,13 +1,18 @@
 import Oscillator from '../lib/Oscillator.js';
 import Mixer from '../lib/Mixer.js';
+import Filter from '../lib/Filter.js';
 
 let AudioContext = window.AudioContext || window.webkitAudioContext;
 let ctx = new AudioContext();
 
 let osc = new Oscillator(ctx);
 let mixer = new Mixer(ctx);
-osc.node.connect(mixer.node);
+let filter = new Filter(ctx);
+
+osc.node.connect(filter.node);
+filter.node.connect(mixer.node);
 mixer.node.connect(ctx.destination);
 
 window.osc = osc;
 window.mixer = mixer;
+window.filter = filter;
