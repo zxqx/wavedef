dde6b36fc5e94eb631762114658537d45f0eeb2e
Zak Angelle <zaknotzak@gmail.com>
2016-01-19 19:49:12 -0600

    Added gulpfile and watch task

---
+++ gulpfile.js
@@ -0,0 +1,32 @@
+var gulp = require('gulp');
+var browserify = require('browserify');
+var babel = require('gulp-babel');
+var babelify = require('babelify');
+var livereload = require('gulp-livereload');
+var source = require('vinyl-source-stream');
+
+gulp.task('compile:example', function() {
+  browserify({ debug: true })
+  .transform(babelify.configure())
+  .require('./examples/main.js', { entry: true })
+  .bundle()
+  .pipe(source('main.js'))
+  .pipe(gulp.dest('dist'))
+  .pipe(livereload());
+});
+
+gulp.task('copy:static', function() {
+  return gulp.src('examples/index.html')
+    .pipe(gulp.dest('dist'))
+    .pipe(livereload());
+});
+
+gulp.task('watch', function() {
+  livereload.listen();
+  gulp.watch('examples/index.html', ['copy:static']);
+  gulp.watch('lib/**/*.js', ['compile:example']);
+});
+
+gulp.task('default', ['client']);
+gulp.task('dev', ['client', 'watch']);
+gulp.task('client', ['copy:static', 'compile:example']);
