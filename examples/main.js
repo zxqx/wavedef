619b59098428c5f26d51b34a23096c17dd640daf
Drake Champagne <trivorak@gmail.com>
2016-01-19 21:55:11 -0600

    Added Mixer That's Not Working

--- examples/main.js
+++ examples/main.js
@@ -1,9 +1,13 @@
 import Oscillator from '../lib/Oscillator.js';
+import Mixer from '../lib/Mixer.js';
 
 let AudioContext = window.AudioContext || window.webkitAudioContext;
 let ctx = new AudioContext();
 
 let osc = new Oscillator(ctx);
-osc.node.connect(ctx.destination);
+let mixer = new Mixer(ctx);
+osc.node.connect(mixer.node);
+mixer.node.connect(ctx.destination);
 
 window.osc = osc;
+window.mixer = mixer;
