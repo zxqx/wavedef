f233c030c3d289bc24956cf0a6ad83c94f834989
Zak Angelle <zaknotzak@gmail.com>
2016-01-20 09:22:48 -0600

    Added index.js which exports all lib modules

--- examples/main.js
+++ examples/main.js
@@ -1,15 +1,12 @@
-import Synth from '../lib/Synth.js';
-import Oscillator from '../lib/Oscillator.js';
-import Mixer from '../lib/Mixer.js';
-import Filter from '../lib/Filter.js';
+import { Synth, Oscillator, Filter, Mixer } from '../lib';
 
 let AudioContext = window.AudioContext || window.webkitAudioContext;
 let ctx = new AudioContext();
 
+let synth = new Synth(ctx);
 let osc = new Oscillator(ctx);
 let filter = new Filter(ctx);
 let mixer = new Mixer(ctx);
-let synth = new Synth(ctx);
 
 synth.addModule(osc);
 synth.addModule(filter);
