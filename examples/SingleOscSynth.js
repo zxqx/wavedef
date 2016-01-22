1c10e08f430b507539512b84c4b8109def7a948e
Zak Angelle <zaknotzak@gmail.com>
2016-01-21 22:02:42 -0600

    Added SingleOscillatorSynth child class

---
+++ examples/SingleOscSynth.js
@@ -0,0 +1,37 @@
+import { Synth, Oscillator, Filter, Mixer, Slider } from '../lib';
+
+export default class SingleOscSynth extends Synth
+{
+  constructor(props)
+  {
+    super(props);
+  }
+
+  on()
+  {
+    let osc = new Oscillator(this.ctx);
+    let filter = new Filter(this.ctx);
+    let mixer = new Mixer(this.ctx);
+
+    this.addModule(osc);
+    this.addModule(filter);
+    this.addModule(mixer);
+    this.connect(osc).to(filter).to(mixer).output();
+
+    let oscFrequencyControl = new Slider(document, 'Osc Freq');
+
+    oscFrequencyControl
+      .setMin(200)
+      .setMax(1500)
+      .setStep(20)
+      .control(osc, osc.setFrequency);
+
+    document.body.appendChild(oscFrequencyControl.el);
+
+    // testing
+    window.synth = this;
+    window.osc = osc;
+    window.filter = filter;
+    window.mixer = mixer;
+  }
+}
--- examples/main.js
+++ examples/main.js
@@ -1,30 +1,7 @@
-import { Synth, Oscillator, Filter, Mixer, Slider } from '../lib';
+import SingleOscSynth from './SingleOscSynth.js';
 
 let AudioContext = window.AudioContext || window.webkitAudioContext;
 let ctx = new AudioContext();
 
-let synth = new Synth(ctx);
-let osc = new Oscillator(ctx);
-let filter = new Filter(ctx);
-let mixer = new Mixer(ctx);
-
-synth.addModule(osc);
-synth.addModule(filter);
-synth.addModule(mixer);
-synth.connect(osc).to(filter).to(mixer).output();
-
-let oscFrequencyControl = new Slider(document, 'Osc Freq');
-
-oscFrequencyControl
-  .setMin(200)
-  .setMax(1500)
-  .setStep(20)
-  .control(osc, osc.setFrequency);
-
-document.body.appendChild(oscFrequencyControl.el);
-
-// testing
-window.osc = osc;
-window.synth = synth;
-window.filter = filter;
-window.mixer = mixer;
+let synth = new SingleOscSynth(ctx);
+synth.on();
