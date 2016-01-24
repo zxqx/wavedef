66faf593c9142639aaa77b43f2e183d6e70149b0
Zak Angelle <zaknotzak@gmail.com>
2016-01-23 19:32:36 -0600

    Added add-child-module tests

--- test/add-child-module.js
+++ test/add-child-module.js
@@ -1,2 +1,21 @@
 import test from 'tape';
 import addChildModule from '../lib/addChildModule.js';
+
+test('Add child module', (t) => {
+  t.plan(3);
+
+  let module = {
+    breakfast: 'isgood',
+    node: {
+      connect: (node) => 'node'
+    }
+  };
+
+  let parentModule = { node: 'node' };
+
+  parentModule::addChildModule('toast', module);
+
+  t.strictEquals(parentModule.children.length, 1);
+  t.strictEquals(parentModule.toast, module);
+  t.throws(() => addChildModule('toast', module));
+});
--- test/oscillator.js
+++ test/oscillator.js
@@ -5,14 +5,14 @@ import Oscillator from '../lib/Oscillator.js';
 test('Start', (t) => {
   t.plan(1);
 
-  var ctx = nm.mock('createOscillator').takes().returns(
+  let ctx = nm.mock('createOscillator').takes().returns(
   {
     start: () => 'start'
   });
 
   Oscillator.__Rewire__('ctx', ctx);
 
-  var osc = new Oscillator();
+  let osc = new Oscillator();
   osc.start();
 
   t.ok(osc.start(), 'should start');
@@ -21,14 +21,14 @@ test('Start', (t) => {
 test('Stop', (t) => {
   t.plan(1);
 
-  var ctx = nm.mock('createOscillator').takes().returns(
+  let ctx = nm.mock('createOscillator').takes().returns(
   {
     stop: () => 'stop'
   });
 
   Oscillator.__Rewire__('ctx', ctx);
 
-  var osc = new Oscillator();
+  let osc = new Oscillator();
   osc.stop();
 
   t.ok(osc.stop(), 'should stop');
@@ -37,13 +37,13 @@ test('Stop', (t) => {
 test('Set frequency', (t) => {
   t.plan(1);
 
-  var ctx = nm.mock('createOscillator').takes().returns({
+  let ctx = nm.mock('createOscillator').takes().returns({
    frequency: { value: 0 }
   });
 
   Oscillator.__Rewire__('ctx', ctx);
 
-  var osc = new Oscillator();
+  let osc = new Oscillator();
   osc.setFrequency(500);
 
   t.strictEquals(500, osc.node.frequency.value);
@@ -52,13 +52,13 @@ test('Set frequency', (t) => {
 test('Set detune', (t) => {
   t.plan(1);
 
-  var ctx = nm.mock('createOscillator').takes().returns({
+  let ctx = nm.mock('createOscillator').takes().returns({
    detune: { value: 0 }
   });
 
   Oscillator.__Rewire__('ctx', ctx);
 
-  var osc = new Oscillator();
+  let osc = new Oscillator();
   osc.setDetune(72);
 
   t.strictEquals(72, osc.node.detune.value);
@@ -67,13 +67,13 @@ test('Set detune', (t) => {
 test('Set waveform type', (t) => {
   t.plan(1);
 
-  var ctx = nm.mock('createOscillator').takes().returns({
+  let ctx = nm.mock('createOscillator').takes().returns({
    type: ''
   });
 
   Oscillator.__Rewire__('ctx', ctx);
 
-  var osc = new Oscillator();
+  let osc = new Oscillator();
   osc.setWaveformType('sawtooth');
 
   t.strictEquals('sawtooth', osc.node.type);
@@ -82,13 +82,13 @@ test('Set waveform type', (t) => {
 test('Get frequency', (t) => {
   t.plan(1);
 
-  var ctx = nm.mock('createOscillator').takes().returns({
+  let ctx = nm.mock('createOscillator').takes().returns({
    frequency: { value: 1500 }
   });
 
   Oscillator.__Rewire__('ctx', ctx);
 
-  var osc = new Oscillator();
+  let osc = new Oscillator();
 
   t.strictEquals(osc.getFrequency(), 1500);
 });
@@ -96,13 +96,13 @@ test('Get frequency', (t) => {
 test('Get detune', (t) => {
   t.plan(1);
 
-  var ctx = nm.mock('createOscillator').takes().returns({
+  let ctx = nm.mock('createOscillator').takes().returns({
    detune: { value: 4 }
   });
 
   Oscillator.__Rewire__('ctx', ctx);
 
-  var osc = new Oscillator();
+  let osc = new Oscillator();
 
   t.strictEquals(osc.getDetune(), 4);
 });
@@ -110,13 +110,13 @@ test('Get detune', (t) => {
 test('Get waveform type', (t) => {
   t.plan(1);
 
-  var ctx = nm.mock('createOscillator').takes().returns({
+  let ctx = nm.mock('createOscillator').takes().returns({
     type: 'sine'
   });
 
   Oscillator.__Rewire__('ctx', ctx);
 
-  var osc = new Oscillator();
+  let osc = new Oscillator();
 
   t.strictEquals(osc.getWaveformType(), 'sine');
 });
