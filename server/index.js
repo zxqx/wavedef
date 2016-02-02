0a948f23dcb09a44b5f831f58d04eb4e739378be
Zak Angelle <zaknotzak@gmail.com>
2016-02-01 22:21:59 -0600

    clean up

--- server/index.js
+++ server/index.js
@@ -2,14 +2,8 @@ var express = require('express');
 var path = require('path');
 var app = express();
 
-var conString = process.env.DATABASE_URL;
 var port = process.env.PORT || 8080;
 
-process.env.PWD = process.cwd();
-
-/**
- * Serve static files
- */
-app.use(express.static(process.env.PWD + '/dist'));
+app.use(express.static(path.join(__dirname, '/dist')));
 
 app.listen(port);
