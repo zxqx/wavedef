b630b19812c940d77d46531fc832cef6c5d704e6
Zak Angelle <zaknotzak@gmail.com>
2016-02-07 12:50:40 -0600

    Added mixer tests

--- test/mixer.js
+++ test/mixer.js
@@ -1,2 +1,30 @@
 import test from 'tape';
+import nm from 'nodemock';
 import Mixer from '../lib/modules/Mixer.js';
+import Gain from '../lib/modules/Gain.js';
+
+test('Mixer', (t) => {
+  t.plan(6);
+
+  let ctx = nm.mock('createChannelMerger').takes(6).returns({
+  mixer: 'node',
+  connect: (node) => node
+  });
+  ctx.mock('createGain').takes().returns(
+  {
+    gain: 'node',
+    connect: (node) => node
+  }).times(6);
+
+  Mixer.__Rewire__('ctx', ctx);
+  Gain.__Rewire__('ctx', ctx);
+
+  let mixer = new Mixer();
+
+  t.ok(mixer.ch1);
+  t.ok(mixer.ch2);
+  t.ok(mixer.ch3);
+  t.ok(mixer.ch4);
+  t.ok(mixer.ch5);
+  t.ok(mixer.ch6);
+});
