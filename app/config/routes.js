0c3b4caf3ca0e003179d15698a97ab81a56a6726
Zak Angelle <zaknotzak@gmail.com>
2016-02-09 01:44:02 -0600

    typo

--- app/config/routes.js
+++ app/config/routes.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Route, IndexRoute } from 'react-router';
-import App from '../containers/app';
+import App from '../containers/App.js';
 import DrumSynthUI from '../containers/DrumSynthUI.js';
 import ThreeOscSynthUI from '../containers/ThreeOscSynthUI.js';
 
