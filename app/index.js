130162509d6a908db9a2fc4fea8e373bc1b8bdc0
Zak Angelle <zaknotzak@gmail.com>
2016-03-12 16:01:42 -0600

    Removed redux infra

--- app/index.js
+++ app/index.js
@@ -1,19 +1,11 @@
-import './preamble.js';
+import 'babel-polyfill';
 import React from 'react';
 import { render } from 'react-dom';
 import { Router } from 'react-router';
-import { Provider } from 'react-redux';
-import { createStore, applyMiddleware } from 'redux';
 import { createHistory } from 'history';
-import thunk from 'redux-thunk';
 import routes from './config/routes.js';
-import rootReducer from './reducers/root.js';
-
-const createStoreWithMiddleware = applyMiddleware(thunk)(createStore);
 
 render(
-  <Provider store={createStoreWithMiddleware(rootReducer)}>
-    <Router history={createHistory()} routes={routes} />
-  </Provider>,
+  <Router history={createHistory()} routes={routes} />,
   document.getElementById('main')
 );
--- app/reducers/oscReducer.js
+++
@@ -1,3 +0,0 @@
-export default function osc(state, action) {
-  return state;
-}
