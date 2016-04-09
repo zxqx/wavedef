15cabe1ad336d8670b7722a73ecdfe201e403139
Zak Angelle <zaknotzak@gmail.com>
2016-04-09 15:49:09 -0500

    Fixed test script

--- webpack.config.production.js
+++ webpack.config.production.js
@@ -44,8 +44,17 @@ module.exports = {
       },
       {
         test: /\.css$/,
-        loader: ExtractTextPlugin.extract('style', 'css?modules&importLoaders=1&localIdentName=[name]__[local]___[hash:base64:5]')
-      }
+        loader: ExtractTextPlugin.extract('style', 'css')
+      },
+      {
+        test: /\.woff(2)?(\?v=[0-9]\.[0-9]\.[0-9])?$/,
+        loader: "url-loader?limit=10000&minetype=application/font-woff"
+      },
+      {
+        test: /\.(ttf|eot|svg)(\?v=[0-9]\.[0-9]\.[0-9])?$/,
+        loader: "file-loader"
+      },
+      { test: /\.(png|jpg)$/, loader: 'url-loader?limit=8192' }
     ]
   },
 };
