3a03261e9c5a169efbe8757bd7297274aa4f5e34
Zak Angelle <zaknotzak@gmail.com>
2016-04-09 18:55:05 -0500

    Hot reload all modules

--- src/index.js
+++ src/index.js
@@ -8,6 +8,10 @@ import 'react-bootstrap-switch/dist/css/bootstrap3/react-bootstrap-switch.css';
 import './style/three-osc-synth.css';
 import './style/index.css';
 
+if (process.env.NODE_ENV === 'development' && module.hot) {
+  module.hot.accept();
+}
+
 render(
   <Router history={browserHistory} routes={routes} />,
   document.getElementById('main')
