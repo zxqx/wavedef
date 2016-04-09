f149b2c0a8676aa868411a7cebee418ec1082dd8
Zak Angelle <zaknotzak@gmail.com>
2016-04-09 15:35:14 -0500

    Initial webpack dev and prod build tooling

--- src/index.js
+++ src/index.js
@@ -3,6 +3,10 @@ import React from 'react';
 import { render } from 'react-dom';
 import { Router, browserHistory } from 'react-router';
 import routes from './config/routes.js';
+import 'bootstrap/dist/css/bootstrap.css';
+import 'react-bootstrap-switch/dist/css/bootstrap3/react-bootstrap-switch.css';
+import '../style/three-osc-synth.css';
+import '../style/index.css';
 
 render(
   <Router history={browserHistory} routes={routes} />,
---
+++ webpack.config.production.js
@@ -0,0 +1,51 @@
+const path = require('path');
+const webpack = require('webpack');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
+
+module.exports = {
+  devtool: 'source-map',
+  entry: './src/index',
+  output: {
+    path: path.join(__dirname, 'dist'),
+    filename: 'bundle.[hash].js',
+    publicPath: '/'
+  },
+  plugins: [
+    new webpack.optimize.OccurenceOrderPlugin(),
+    new webpack.DefinePlugin({
+      'process.env': {
+        NODE_ENV: JSON.stringify('production')
+      }
+    }),
+    new webpack.optimize.UglifyJsPlugin({
+      compressor: {
+        screw_ie8: true,
+        warnings: false
+      }
+    }),
+    new HtmlWebpackPlugin({
+      template: './build/index.html',
+      filename: 'index.html'
+    }),
+    new ExtractTextPlugin('index.css', {
+      allChunks: true
+    })
+  ],
+  resolve: {
+    extensions: ['', '.js']
+  },
+  module: {
+    loaders: [
+      {
+        test: /\.js$/,
+        loaders: ['babel'],
+        exclude: /node_modules/,
+      },
+      {
+        test: /\.css$/,
+        loader: ExtractTextPlugin.extract('style', 'css?modules&importLoaders=1&localIdentName=[name]__[local]___[hash:base64:5]')
+      }
+    ]
+  },
+};
