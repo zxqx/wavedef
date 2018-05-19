4ef8a5a563486481ff8d0935761da40e28c34e07
Drake Champagne <trivorak@gmail.com>
2018-05-19 17:36:47 -0500

    Added Test for all Set Methods
    All set methods have a test for the Ringmod object. Connection and Disconnect is not Added yet in this test

--- src/modules/Ringmod.test.js
+++ src/modules/Ringmod.test.js
@@ -21,4 +21,37 @@ describe ('Ringmod', () => {
 
     expect(ringmod.lfo.getFrequency()).toEqual(525);
   });
+
+  it('should set depth', () => {
+    const ringmod = new Ringmod();
+
+    ringmod.setDepth(0.53);
+
+    expect(ringmod.lfo.gain.getGain()).toEqual(0.53)
+  });
+
+  it('should set gain', () => {
+    const ringmod = new Ringmod();
+
+    ringmod.setGain(0.25);
+
+    expect(ringmod.gain.getGain()).toEqual(0.25)
+  });
+
+  it('should set wet/dry mix', () => {
+    const ringmod = new Ringmod();
+
+    ringmod.setWetDryMix(0.35);
+
+    expect(ringmod.post.getGain()).toEqual(0.35);
+    expect(ringmod.dry.getGain()).toEqual(0.65);
+  });
+
+  it('should set waveform', () => {
+    const ringmod = new Ringmod();
+
+    ringmod.setWaveformType('sine');
+
+    expect(ringmod.lfo.osc.node.type).toEqual('sine');
+  })
 })
