f75cbe17e08fda2cee3ccbb077aae8d8689c438b
Zak Angelle <zaknotzak@gmail.com>
2018-06-16 12:23:14 -0500

    Refactor apply params

--- src/helpers/applyParams.js
+++ src/helpers/applyParams.js
@@ -6,11 +6,15 @@ function getParamValue(value, defaultValue) {
   return value !== undefined ? value : defaultValue;
 }
 
+function getSetParamMethodName(param) {
+  return `set${capitalize(param)}`;
+}
+
 export default function applyParams(params) {
   Object.keys(this.defaults)
     .forEach((param) => {
       const value = getParamValue(params[param], this.defaults[param]);
-      const setParamMethodName = `set${capitalize(param)}`;
+      const setParamMethodName = getSetParamMethodName(param);
       this::this[setParamMethodName](value);
     });
 }
