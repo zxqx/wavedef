aa164faf3ddae8f51f59410da6604f1eb405458e
Zak Angelle <zaknotzak@gmail.com>
2018-06-16 12:45:52 -0500

    Set up chorus default params

--- src/example/Chorus/index.js
+++ src/example/Chorus/index.js
@@ -1,3 +1,4 @@
+/* eslint-disable */
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import stereoPannerNode from 'stereo-panner-node';
@@ -16,51 +17,7 @@ export default class Chorus extends Component {
 
     return (
       <AudioControlGroup label="Chorus">
-
-        <Slider
-          label="Offset 1"
-          defaultValue={0.006}
-          min={0.001}
-          max={0.1}
-          step={0.001}
-          onChange={chorus::chorus.setOffset1}
-        />
-
-        <Slider
-          label="Offset 2"
-          defaultValue={0.020}
-          min={0.001}
-          max={0.1}
-          step={0.001}
-          onChange={chorus::chorus.setOffset2}
-        />
-
-        <Slider
-          label="Width"
-          defaultValue={0}
-          min={0}
-          max={1}
-          step={0.01}
-          onChange={chorus::chorus.setWidth}
-        />
-
-        <Slider
-          label="Movement"
-          defaultValue={0.005}
-          min={0.05}
-          max={1}
-          step={0.001}
-          onChange={chorus::chorus.setMovement}
-        />
-
-        <Slider
-          label="Mix"
-          defaultValue={0}
-          min={0}
-          max={1}
-          step={0.001}
-          onChange={chorus::chorus.setWetDryMix}
-        />
+        chorus
       </AudioControlGroup>
     );
   }
--- src/modules/Chorus.js
+++ src/modules/Chorus.js
@@ -2,9 +2,20 @@ import Delay from './Delay';
 import Gain from './Gain';
 import Panner from './Panner';
 import LFO from './LFO';
+import applyParams from '../helpers/applyParams';
 
 export default class Chorus {
-  constructor() {
+  constructor(params = {}) {
+    this.defaults = {
+      offset1DelayTime: 0.006,
+      offset1WetDryMix: 1,
+      offset2DelayTime: 0.02,
+      offset2WetDryMix: 1,
+      movement: 0.005,
+      width: 0.2,
+      wetDryMix: 1,
+    };
+
     this.input = new Gain();
     this.direct = new Gain();
     this.output = new Gain();
@@ -42,28 +53,37 @@ export default class Chorus {
     offset2pan.node.connect(mix.node);
     mix.node.connect(output.node);
 
-    offset1.setFeedback(0);
-    offset1.setWetDryMix(1);
-    offset1.setDelayTime(0.0006);
     offset1pan.setPanPosition(-0.5);
-
-    offset2.setFeedback(0);
-    offset2.setWetDryMix(1);
-    offset2.setDelayTime(0.001);
     offset2pan.setPanPosition(0.05);
-
+    offset1.setFeedback(0);
+    offset2.setFeedback(0);
     mix.setGain(1);
 
     lfo.modulate(this.offset1.delay.delayTime);
     lfo.modulate(this.offset2.delay.delayTime);
-
     lfo.setDepth(0.01);
-    lfo.setFrequency(0.005);
+
+    this::applyParams(params);
   }
 
-  setWetDryMix(mix) {
-    this.direct.setGain(1 - mix);
-    this.mix.setGain(mix / 2);
+  setOffset1DelayTime(time) {
+    this.offset1.setDelayTime(time);
+  }
+
+  setOffset2DelayTime(time) {
+    this.offset2.setDelayTime(time);
+  }
+
+  setOffset1WetDryMix(mix) {
+    this.offset1.setWetDryMix(mix);
+  }
+
+  setOffset2WetDryMix(mix) {
+    this.offset2.setWetDryMix(mix);
+  }
+
+  setMovement(amount) {
+    this.lfo.setFrequency(amount);
   }
 
   setWidth(width) {
@@ -71,15 +91,8 @@ export default class Chorus {
     this.offset2pan.setPanPosition(width);
   }
 
-  setOffset1(time) {
-    this.offset1.setDelayTime(time);
-  }
-
-  setOffset2(time) {
-    this.offset2.setDelayTime(time);
-  }
-
-  setMovement(amount) {
-    this.lfo.setFrequency(amount);
+  setWetDryMix(mix) {
+    this.direct.setGain(1 - mix);
+    this.mix.setGain(mix / 2);
   }
 }
