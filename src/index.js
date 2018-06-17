f86c36451ab8e0c15e2d200fbdffd934b7897b8a
Zak Angelle <zaknotzak@gmail.com>
2018-06-17 12:35:11 -0500

    Update index file

--- src/index.js
+++ src/index.js
@@ -1,3 +1,43 @@
 import Cyanide from './stock-synths/Cyanide';
+import Chorus from './modules/Chorus';
+import ComputerKeyboard from './modules/ComputerKeyboard';
+import Delay from './modules/Delay';
+import Envelope from './modules/Envelope';
+import Filter from './modules/Filter';
+import FrequencyAnalyzer from './modules/FrequencyAnalyzer';
+import Gain from './modules/Gain';
+import LFO from './modules/LFO';
+import MIDIController from './modules/MIDIController';
+import Mixer from './modules/Mixer';
+import MixerChannel from './modules/MixerChannel';
+import Oscillator from './modules/Oscillator';
+import Panner from './modules/Panner';
+import Ringmod from './modules/Ringmod';
+import Sequencer from './modules/Sequencer';
+import Synth from './modules/Synth';
+import TunaOverdrive from './modules/TunaOverdrive';
+import VCA from './modules/VCA';
+import WhiteNoise from './modules/WhiteNoise';
 
-export { Cyanide };
+export {
+  Cyanide,
+  Chorus,
+  ComputerKeyboard,
+  Delay,
+  Envelope,
+  Filter,
+  FrequencyAnalyzer,
+  Gain,
+  LFO,
+  MIDIController,
+  Mixer,
+  MixerChannel,
+  Oscillator,
+  Panner,
+  Ringmod,
+  Sequencer,
+  Synth,
+  TunaOverdrive,
+  VCA,
+  WhiteNoise,
+};
