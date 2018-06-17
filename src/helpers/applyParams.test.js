a03af053d513d814667ce2cef288a9d5608a17cd
Zak Angelle <zaknotzak@gmail.com>
2018-06-16 21:34:58 -0500

    Add apply params tests

---
+++ src/helpers/applyParams.test.js
@@ -0,0 +1,28 @@
+import applyParams from './applyParams';
+
+describe('Apply params helper', () => {
+  it('should apply params', () => {
+    const envelope = {
+      defaults: {
+        attack: 0,
+        decay: 0.1,
+        sustain: 1,
+        release: 0.5,
+      },
+      setAttack: attack => envelope.attack = attack,
+      setDecay: decay => envelope.decay = decay,
+      setSustain: sustain => envelope.sustain = sustain,
+      setRelease: release => envelope.release = release,
+    };
+
+    envelope::applyParams({
+      attack: 1,
+      sustain: 0.6,
+    });
+
+    expect(envelope.attack).toEqual(1);
+    expect(envelope.decay).toEqual(0.1);
+    expect(envelope.sustain).toEqual(0.6);
+    expect(envelope.release).toEqual(0.5);
+  });
+});
